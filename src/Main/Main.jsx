19143bc0ee9c45ae6493267b31a3d41ea9b4218d
Antonio Marmolejo <djcamg71@gmail.com>
2025-01-08 15:33:31 -0500

    fix: se mueven los dos componentes Card, y el Popup dentro de components

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
 import pencil from "../images/Vector-pencil2.svg";
 import imagenSuma from "../images/Vector-suma-agregar.svg";
-import Popup from "../components/Popup";
+import Popup from "../components/Main/components/Popup";
 import EditProfile from "../components/EditProfile/EditProfile";
 import Avatar from "../components/EditAvatar/EditAvatar";
 import NewCard from "../components/NewCard/NewCard";
-import Cards from "../components/Card";
+import Cards from "../components/Main/components/Card";
 import ImagePopup from "../components/ImagePopup/ImagePopup";
 import DeleteCard from "../components/RemoveCard/RemoveCard";
 import CurrentUserContext from "../contexts/CurrentUserContext";
