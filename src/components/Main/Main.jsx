3cc1a8fb0eaa49f65f83bd9c00bc0f95d60be9d3
Antonio Marmolejo <djcamg71@gmail.com>
2025-01-08 00:24:05 -0500

    fix: corrección del directorio se mueven los componentes de las ventanas emergentes al directorio components que está dentro de Main.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,12 @@ import React, { useState, useContext } from "react";
 import pencil from "../../images/Vector-pencil2.svg";
 import imagenSuma from "../../images/Vector-suma-agregar.svg";
 import Popup from "./components/Popup/Popup";
-import EditProfile from "./components/Popup/EditProfile/EditProfile";
-import Avatar from "./components/Popup/EditAvatar/EditAvatar";
-import NewCard from "./components/Popup/NewCard/NewCard";
+import EditProfile from "./components/EditProfile/EditProfile";
+import Avatar from "./components/EditAvatar/EditAvatar";
+import NewCard from "./components/NewCard/NewCard";
 import Cards from "./components/card/Card";
-import ImagePopup from "./components/Popup/ImagePopup/ImagePopup";
-import DeleteCard from "./components/Popup/RemoveCard/RemoveCard";
+import ImagePopup from "./components/ImagePopup/ImagePopup";
+import DeleteCard from "./components/RemoveCard/RemoveCard";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 export default function Main({
