97d955adf9199ecb701ef4a8a59e6909ea8922a9
Antonio Marmolejo <djcamg71@gmail.com>
2025-01-08 13:49:53 -0500

    fix: corrección en el sistema de archivos se vuelve a crear el components y se agrega el componente App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Header from "./Header/Header";
-import Main from "../Main";
-import Footer from "./Footer/Footer";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
-import { api } from "../../utils/Api";
+import Header from "../Main/components/Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Main/components/Footer/Footer";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+import { api } from "../utils/Api";
 
 export default function App() {
     const [currentUser, setCurrentUser] = useState({});
